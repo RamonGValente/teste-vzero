47e81d7dae0539a20807335b0289495e1fca4140
RamonGValente <sistemasrtr@gmail.com>
2025-12-19 23:56:33 -0300

    V2.6

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1896,7 +1896,7 @@ export default function WorldFlow() {
         </div>
       </div>
 
-      {/* Conteúdo principal */}
+      {/* Conteúdo principal rgv*/}
       <div className="w-full h-full pt-16 sm:pt-20 bg-black">
         {renderContent()}
       </div>
